290d8a57bf433170e6a3d4f8cd38dd3f91bf8ba4
SandeshOnRails <basnetsandesh450@gmail.com>
2018-12-17 16:23:38 -0800

    added constants for all game messages

---
+++ constants/events.js
@@ -0,0 +1,23 @@
+const PLAYER_JOINED = 'player joined'
+const UPDATEGAMELIST = 'update game list'
+const STARTGAME = 'start game'
+const WITHDRAW_CARD = 'withdraw card'
+const WELCOME = 'welcome'
+const WAIT = 'wait for other players'
+const UPDATE_CLIENT = 'update request client'
+const UPDATE_SERVER = 'update request server'
+const CARDS_MELDED = 'cards melded'
+const UPDATE = 'update request'
+const DISCARD_CARD = 'card discarded'
+const SUCCESS = 'success'
+const WIN = 'game won'
+const TIE = 'game tie'
+const SUCCESSFUL_MELD = 'successful meld'
+const FAILED_MELD = 'failed meld'
+const PICKED_MELD_CARD = 'picked meld card'
+const PICKED_MELD_SUCCESS = 'picked meld success'
+const CARDS_LAYOFF = 'cards layoff'
+const GAME_MESSAGE = 'game message'
+
+
+module.exports = { PLAYER_JOINED, UPDATEGAMELIST, STARTGAME, WITHDRAW_CARD, WELCOME, WAIT, UPDATE_CLIENT, UPDATE_SERVER, CARDS_MELDED, UPDATE , SUCCESS, DISCARD_CARD, SUCCESSFUL_MELD, FAILED_MELD, PICKED_MELD_CARD, PICKED_MELD_SUCCESS, CARDS_LAYOFF, WIN, TIE, GAME_MESSAGE  }
---
+++ constants/gameMessages.js
@@ -0,0 +1,14 @@
+module.exports = Object.freeze({
+MSG_WITHDRAW_CARD : 'Discard a card OR click on start meld to meld cards.',
+MSG_WAIT : 'Welcome ! Waiting for other player to join.',
+MSG_DISCONNECT : 'Other Player got disconnected! Wait or return to lobby.',
+MSG_WIN : 'Congratulations!!!!! You won the game.',
+MSG_LOST : 'You Lost',
+MSG_TIE : 'Game is a Tie',
+MSG_SUCCESSFUL_MELD : 'Cards melded. Your Turn Again! Discard or meld cards.',
+MSG_FAILED_MELD : 'Meld failed. Melded cards should be of same suit sequence OR same numbers.',
+MSG_CARDS_LAYOFF_SUCCESS : 'cards layoff successful',
+MSG_CARDS_LAYOFF_FAIL : 'cards layoff not successful',
+MSG_UNAUTHORIZED : 'You cannot join this game. Please go to lobby!'
+
+});
